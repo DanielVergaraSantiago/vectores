3854c11349a3168638031bf4a5cebec91359b5aa
Daniel Vergara S. <dvergara066@gmail.com>
2017-03-04 20:55:52 -0500

    Creo algoritmo que a traves de un menu permite a un verctor ingresado añadir o eliminar elementos

---
+++ src/añadiryeliminar/AñadiryEliminar.java
@@ -0,0 +1,79 @@
+package añadiryeliminar;
+
+import java.util.Scanner;
+public class AñadiryEliminar {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int[] vector = new int[100];
+        int eliminar, tam, seleccion;
+        
+        System.out.print("Ingrese tamaño del vector: ");
+        tam = leer.nextInt();
+        
+        for(int i=0; i<tam ;i++){
+            System.out.print("Ingrese el "+(i+1)+" elemento del vector: ");
+            vector[i] = leer.nextInt();
+        }
+        
+        System.out.println("Elja la opcion deseada: ");
+        System.out.println(" ");
+        System.out.println("*******************************************");
+        System.out.println("*    1. Eliminar un elemento del vector.  *");
+        System.out.println("*    2. Agregar un elemento al vector.    *");
+        System.out.println("*    3. Salir.                            *");
+        System.out.println("*******************************************");
+        seleccion = leer.nextInt();
+        
+        
+        switch(seleccion){
+            case(1):
+                System.out.print("Ingrese elemento a eliminar: ");
+                eliminar = leer.nextInt();
+                
+                for(int i=0 ; i<tam ;i++){
+                    if(eliminar == vector[i]){
+                        for(int j=i ;j<tam ;j++){
+                            vector[j] = vector[j+1];
+                        }
+                        tam = tam-1;
+                    }
+                }
+                
+                System.out.println("Vector resultante: ");
+                for(int i=0; i<tam ;i++){
+                    System.out.print(vector[i]+" ");
+                }
+                
+                break;
+                
+            case(2):
+                System.out.print("Ingrese elemento a agregar: ");
+                int agregar = leer.nextInt();
+                
+                vector[tam] = agregar;
+                tam = tam+1;
+                for(int i=0 ;i<tam ;i++){
+                    for(int j=0; j<tam ; j++){
+                        if(vector[i]<vector[j]){
+                            int tmp = vector[i];
+                            vector[i] = vector[j];
+                            vector[j] = tmp;
+                        }
+                    }
+                }
+                
+                System.out.println("Vector resultante: ");
+                for(int i=0; i<tam ;i++){
+                    System.out.print(vector[i]+" ");
+                }
+                
+                break;
+                
+            case(3):
+                break;
+        }
+        
+    }
+    
+}
